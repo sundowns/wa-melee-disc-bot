45d38bd0709056d996a7bbf8cf0322a680de752f
Sundowns <tom@smallridge.com.au>
2020-02-28 17:58:30 +0800

    update PR command

--- misc.js
+++ misc.js
@@ -1,22 +1,32 @@
-const prefix = '.'
+const prefix = ".";
 
 module.exports = {
-    MessageHandler : function(lowercaseContent, msg) {
-        if (lowercaseContent === prefix + 'pr' || lowercaseContent === prefix + 'prs') {
-            msg.reply("\n https://i.imgur.com/vQ6y16b.png \n https://i.imgur.com/PtbHt4H.png");
-        } else if (lowercaseContent === prefix + 'stream' || lowercaseContent === prefix + 'streams') {
-            msg.reply("\n https://www.twitch.tv/perthsmash \n https://www.youtube.com/channel/UCl8p1RKYAHgdgDDPS49-bJQ");
-        } else if (lowercaseContent === prefix + 'bracket' || lowercaseContent === prefix + 'brackets') {
-            msg.reply("\n http://challonge.com/users/maribro \n http://challonge.com/users/perthsmash \n http://challonge.com/users/curtinsmash \n http://challonge.com/users/sundowns (netplay)");
-        } else if (lowercaseContent.startsWith(prefix + 'f')) {
-            var result = msg.content.match(/\.f (.+)/);
-            if (result) {
-                msg.channel.send(`Press 🇫 to pay respects to ${result[1]}`).then(message => message.react("🇫"));
-            } else if (lowercaseContent === ".f") {
-                msg.channel.send("Press 🇫 to pay respects").then(message => message.react("🇫"));
-            }
-        } else if (lowercaseContent === prefix + 'rule1') {
-             msg.reply("\n http://imgur.com/a/NsNqI");
-        }
+  MessageHandler: function(lowercaseContent, msg) {
+    if (
+      lowercaseContent === prefix + "pr" ||
+      lowercaseContent === prefix + "prs"
+    ) {
+      msg.reply("\n https://i.imgur.com/ui33zag.jpg");
+    } else if (
+      lowercaseContent === prefix + "stream" ||
+      lowercaseContent === prefix + "streams"
+    ) {
+      msg.reply(
+        "\n https://www.twitch.tv/perthsmash \n https://www.youtube.com/channel/UCl8p1RKYAHgdgDDPS49-bJQ"
+      );
+    } else if (lowercaseContent.startsWith(prefix + "f")) {
+      var result = msg.content.match(/\.f (.+)/);
+      if (result) {
+        msg.channel
+          .send(`Press 🇫 to pay respects to ${result[1]}`)
+          .then(message => message.react("🇫"));
+      } else if (lowercaseContent === ".f") {
+        msg.channel
+          .send("Press 🇫 to pay respects")
+          .then(message => message.react("🇫"));
+      }
+    } else if (lowercaseContent === prefix + "rule1") {
+      msg.reply("\n http://imgur.com/a/NsNqI");
     }
-}
+  }
+};
